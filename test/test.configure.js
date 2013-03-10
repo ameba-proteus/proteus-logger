ec5a05e3ce0f52624cce869321694e9d67c50551
suguru <snamura@gmail.com>
2013-03-10 18:48:56 +0900

    Remove logger from dependencies.

--- test/test.configure.js
+++ test/test.configure.js
@@ -1,11 +1,10 @@
 
 var proteusLogger = require('../lib/logger');
-var configurator = require('proteus-configurator');
 var should = require('should');
 
 describe('logger', function() {
 	describe('#configure', function() {
-		it('can configure', function() {
+		it('can configure', function(done) {
 			proteusLogger.configure({
 				appenders: {
 					con: {
@@ -19,16 +18,12 @@ describe('logger', function() {
 				}
 			});
 			var logger = proteusLogger.get('category1');
-			console.log();
 			logger.info('test test test');
 			logger.error('test test test', new Error("YOYO"));
 			logger.warn('test test test');
 			logger.debug('test test test');
 			logger.fatal('test test test');
-		});
-		it('can configure from configurator', function() {
-			configurator.configure(__dirname + '/config/logger.json');
-			var logger = proteusLogger.get('category1');
+			done();
 		});
 	});
 });
